fc30acee244b815d61d0307686208835efd721cf
Mustafa Shaheen <68672770+musalem@users.noreply.github.com>
2026-04-18 00:04:35 +0200

    update

---
+++ download.js
@@ -0,0 +1,18 @@
+const DOWNLOADS = {
+
+netcontrol:{
+    version:"1.0",
+    url:"https://example.com/netcontrol.zip"
+},
+
+securedesk:{
+    version:"2.1",
+    url:"https://example.com/securedesk.zip"
+},
+
+dataflow:{
+    version:"1.5",
+    url:"https://example.com/dataflow.zip"
+}
+
+};
